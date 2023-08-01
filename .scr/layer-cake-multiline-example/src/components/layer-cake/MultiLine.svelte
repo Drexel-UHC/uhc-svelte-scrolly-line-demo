bcb6b654b4576053480316a79f28569cd8eb501b
rl627 <rl627@drexel.edu>
2023-08-01 15:45:08 -0400

    update transition in MultLine with svelte fade !

--- .scr/layer-cake-multiline-example/src/components/layer-cake/MultiLine.svelte
+++ .scr/layer-cake-multiline-example/src/components/layer-cake/MultiLine.svelte
@@ -1,6 +1,6 @@
 <script>
   import { getContext } from 'svelte';
-
+  import { fade } from 'svelte/transition';
   const {
     data,
     xGet,
@@ -15,6 +15,16 @@
   } = getContext('LayerCake');
 
   $: path = (values) => {
+    console.log(`values`);
+    console.log(values);
+    const results =
+      'M' +
+      values
+        .map((d) => {
+          return $xGet(d) + ',' + $yGet(d);
+        })
+        .join('L');
+    // console.log(results);
     return (
       'M' +
       values
@@ -29,7 +39,12 @@
 
 <g class="line-group">
   {#each $data as group}
-    <path class="path-line" d={path(group.values)} stroke={$zGet(group)} />
+    <path
+      class="path-line"
+      d={path(group.values)}
+      stroke={$zGet(group)}
+      transition:fade={{ duration: 500 }}
+    />
   {/each}
 </g>
 
